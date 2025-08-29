f927893bb81d8aa1cc7f489825c55dbc430915e8
Gabriel Abreu <gabriel@gabbium.dev>
2025-08-29 00:42:32 -0300

    feat: add paginated list (#8)

---
+++ src/CleanArch/PaginatedList.cs
@@ -0,0 +1,21 @@
+﻿namespace CleanArch;
+
+public record PaginatedList<T>
+{
+    public IReadOnlyList<T> Items { get; }
+    public int PageNumber { get; }
+    public int PageSize { get; }
+    public int TotalItems { get; }
+    public int TotalPages { get; }
+    public bool HasPreviousPage => PageNumber > 1;
+    public bool HasNextPage => PageNumber < TotalPages;
+
+    public PaginatedList(IReadOnlyList<T> items, int totalItems, int pageNumber, int pageSize)
+    {
+        Items = items;
+        TotalItems = totalItems;
+        PageNumber = pageNumber;
+        PageSize = pageSize;
+        TotalPages = (int)Math.Ceiling(totalItems / (double)pageSize);
+    }
+}
