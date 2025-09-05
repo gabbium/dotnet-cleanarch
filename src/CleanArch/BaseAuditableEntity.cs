98984918d3579f2cf3dc7a5b36d362abc6314bde
Gabriel Abreu <gabriel@gabbium.dev>
2025-09-05 13:58:25 -0300

    fix: nullable audit by props

--- src/CleanArch/BaseAuditableEntity.cs
+++ src/CleanArch/BaseAuditableEntity.cs
@@ -4,9 +4,9 @@ public abstract class BaseAuditableEntity : BaseEntity
 {
     public DateTimeOffset CreatedOn { get; set; }
 
-    public Guid CreatedBy { get; set; }
+    public Guid? CreatedBy { get; set; }
 
     public DateTimeOffset LastModifiedOn { get; set; }
 
-    public Guid LastModifiedBy { get; set; }
+    public Guid? LastModifiedBy { get; set; }
 }
