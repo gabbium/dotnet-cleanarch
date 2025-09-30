217735bc242ac4cc8c33ccbac0f487510ac36d3e
Gabriel Abreu <gabriel@gabbium.dev>
2025-09-29 21:54:03 -0300

    feat: paginated list docs (#15)
    * feat: add paginatad list docs
    
    * chore: add using

--- src/CleanArch/GlobalUsings.cs
+++ src/CleanArch/GlobalUsings.cs
@@ -1,3 +1,4 @@
-﻿global using FluentValidation;
+global using System.ComponentModel;
+global using FluentValidation;
 global using Microsoft.Extensions.DependencyInjection;
 global using Microsoft.Extensions.Logging;
